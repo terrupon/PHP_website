455d34ade686aa9a269a2a0f62929a359b122fba
terrupon <tryumashev@mail.ru>
2023-05-28 19:55:59 +0500

    Create styles.php

---
+++ styles.php
@@ -0,0 +1,33 @@
+.container {
+    background-color: #f2f2f2;
+    width: 80%;
+    margin: 0 auto;
+    padding: 20px;
+    text-align: center;
+}
+
+h1 {
+    font-size: 32px;
+    text-transform: uppercase;
+}
+
+h2 {
+    font-size: 24px;
+    margin-top: 40px;
+}
+
+h3 {
+    font-size: 20px;
+    margin-top: 20px;
+}
+
+.info-block {
+    background-color: #e6e6e6;
+    padding: 20px;
+    margin-top: 20px;
+}
+
+img {
+    width: 200px;
+    margin-top: 40px;
+}
